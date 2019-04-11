70615e1339481e3fe47b5d2567b5e744478d3569
Tim <sarrtim@gmail.com>
2019-04-11 21:30:32 +0300

    Add ContributionController

---
+++ app/Http/Controllers/ContributionController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContributionController extends Controller
+{
+    public function index()
+    {
+        return Contribution::all();
+    }
+
+    public function show(Contribution $contribution)
+    {
+        return $contribution;
+    }
+
+    public function store(Request $request)
+    {
+        $contribution = Contribution::create($request->all());
+
+        return response()->json($contribution, 201);
+    }
+
+    public function update(Request $request, Gender $contribution)
+    {
+        $contribution->update($request->all());
+
+        return response()->json($contribution, 200);
+    }
+
+    public function delete(Contribution $contribution)
+    {
+        $contribution->delete();
+
+        return response()->json(null, 204);
+    }
+}
