8adf7e88711e5a894242a8db3ce352333373d826
Tim <sarrtim@gmail.com>
2019-04-11 20:35:56 +0300

    Add relations

--- app/ChildSponsor.php
+++ app/ChildSponsor.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class ChildSponsor extends Model
 {
     protected $fillable = ['child_id', 'sponsor_id'];
+
+    public function sponsor(){
+        return $this->belongsTo('App\Sponsor');
+    }
+
+    public function child(){
+        return $this->belongsTo('App\Child');
+    }
 }
