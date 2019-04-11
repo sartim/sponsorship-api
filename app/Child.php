8aa6f22dc1043dc1a7a69d7da8e30fcdf12dfc5a
Tim <sarrtim@gmail.com>
2019-04-11 20:27:50 +0300

    Add relationships

--- app/Child.php
+++ app/Child.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Child extends Model
 {
     protected $fillable = ['first_name', 'last_name', 'date_of_birth', 'gender_id'];
+
+    public function sponsor()
+    {
+        return $this->hasOne(ChildSponsor::class);
+    }
+
+    public function gender(){
+        return $this->belongsTo('App\Gender');
+    }
 }
