dcff3b592debccf0e8d4e13d8b6daa66858e8a52
Tim <sarrtim@gmail.com>
2019-04-11 14:09:43 +0300

    Add register & registered & update create

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\User;
 use App\Http\Controllers\Controller;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
@@ -49,9 +51,12 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'phone' => ['required', 'string', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'is_active' => ['required'],
         ]);
     }
 
@@ -64,9 +69,42 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
+            'first_name' => $data['first_name'],
+            'last_name' => $data['last_name'],
+            'phone' => $data['phone'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'is_active' => $data['is_active'],
         ]);
+    }
+
+    public function register(Request $request)
+    {
+        // Here the request is validated. The validator method is located
+        // inside the RegisterController, and makes sure the name, email
+        // password and password_confirmation fields are required.
+        $this->validator($request->all())->validate();
+
+        // A Registered event is created and will trigger any relevant
+        // observers, such as sending a confirmation email or any
+        // code that needs to be run as soon as the user is created.
+        event(new Registered($user = $this->create($request->all())));
+
+        // After the user is created, he's logged in.
+        $this->guard()->login($user);
+
+        // And finally this is the hook that we want. If there is no
+        // registered() method or it returns null, redirect him to
+        // some other URL. In our case, we just need to implement
+        // that method to return the correct response.
+        return $this->registered($request, $user)
+            ?: redirect($this->redirectPath());
+    }
+
+    protected function registered(Request $request, $user)
+    {
+        $user->generateToken();
+
+        return response()->json(['data' => $user->toArray()], 201);
     }
 }
