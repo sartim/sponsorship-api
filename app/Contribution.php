5000305bc46bb57e969e88060b257304d11b0776
Tim <sarrtim@gmail.com>
2019-04-11 21:21:48 +0300

    Add currency_id to fillable

--- app/Contribution.php
+++ app/Contribution.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Contribution extends Model
 {
-    protected $fillable = ['child_id', 'sponsor_id', 'contribution'];
+    protected $fillable = ['child_id', 'sponsor_id', 'currency_id', 'contribution'];
 }
