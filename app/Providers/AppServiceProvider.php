26c51b0c597de48f7a8bf54c192988b8f83a28c8
Tim <sarrtim@gmail.com>
2019-04-12 23:52:46 +0300

    Add macro for search filtering

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,3 +27,7 @@ class AppServiceProvider extends ServiceProvider
         //
     }
 }
+
+Builder::macro('whereLike', function(string $attribute, string $searchTerm) {
+    return $this->orWhere($attribute, 'LIKE', "{$searchTerm}%");
+});
