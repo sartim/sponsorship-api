4617e57319e04bd14581a568409ff552186f4571
Tim <sarrtim@gmail.com>
2019-04-13 14:30:22 +0300

    Add relations

--- app/Contribution.php
+++ app/Contribution.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Contribution extends Model
 {
     protected $fillable = ['child_id', 'sponsor_id', 'currency_id', 'contribution'];
+
+    public function sponsor(){
+        return $this->belongsTo('App\Sponsor');
+    }
+
+    public function child(){
+        return $this->belongsTo('App\Child');
+    }
 }
