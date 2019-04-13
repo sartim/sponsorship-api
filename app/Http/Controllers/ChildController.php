e68a3307846a2b704b2a003947c7cc8967dc806e
Tim <sarrtim@gmail.com>
2019-04-13 18:07:08 +0300

    Refactor

--- app/Http/Controllers/ChildController.php
+++ app/Http/Controllers/ChildController.php
@@ -8,6 +8,10 @@ use Illuminate\Support\Facades\DB;
 
 class ChildController extends Controller
 {
+    /**
+     * Retrieve Children list
+     * @return \Illuminate\Database\Eloquent\Collection|static[]
+     */
     public function index()
     {
         $children = Child::with(['sponsor.sponsor','gender'])->get();
@@ -16,44 +20,60 @@ class ChildController extends Controller
         return $children;
     }
 
-    public function yearlyContribution(Request $request) {
+    /**
+     * Retrieve Child monthly contributions list by year
+     * @param Request $request
+     * @return array
+     */
+    public function monthlyContribution(Request $request)
+    {
         $child_id = $request->query('child_id');
-        $data = DB::table('contributions')
-            ->select(DB::raw("SUM(contribution) as contribution, extract(year from created_at) AS year"))
-            ->whereRaw('child_id = ?',[$child_id])
-            ->groupBy(DB::raw("year"))
-            ->get();
-
-        $output = array();
-        foreach($data as $obj){
-            array_push($output, array(
-                    'year' =>intval($obj->year),
-                    'contribution' => intval($obj->contribution)
-                )
-            );
-
-        }
-        return $output;
+        $year = $request->query('year');
+        $data = (new Child)->getMonthlyContributions($child_id, $year);
+        return $data;
     }
 
+    /**
+     * Retrieve Child yearly contributions list
+     * @param Request $request
+     * @return array
+     */
+    public function yearlyContribution(Request $request)
+    {
+        $child_id = $request->query('child_id');
+        $data = (new Child)->getYearlyContribution($child_id);
+        return $data;
+    }
+
+    /**
+     * Search for Child
+     * @param Request $request
+     * @return mixed
+     */
     public function search(Request $request)
     {
         $search = $request->query('q');
         # TODO Update to Full text search using something like elastic search
-        $query= Child::query()
-            ->with(['gender'])
-            ->whereLike('first_name', $search)
-            ->whereLike('last_name', $search)
-            ->get();
+        $query = (new Child)->search($search);
         return $query;
     }
 
+    /**
+     * Get child by id
+     * @param Child $child
+     * @return \Illuminate\Database\Eloquent\Collection|\Illuminate\Database\Eloquent\Model|null|static|static[]
+     */
     public function show(Child $child)
     {
         $r = Child::with(['gender'])->find($child['id']);
         return $r;
     }
 
+    /**
+     * Save POST data to create Child
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function store(Request $request)
     {
         $child = Child::create($request->all());
@@ -61,6 +81,12 @@ class ChildController extends Controller
         return response()->json($child, 201);
     }
 
+    /**
+     * Save PUT data to update child
+     * @param Request $request
+     * @param Child $child
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function update(Request $request, Child $child)
     {
         $child->update($request->all());
@@ -68,6 +94,12 @@ class ChildController extends Controller
         return response()->json($child, 200);
     }
 
+    /**
+     * Remove DELETE data to delete child
+     * @param Child $child
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Exception
+     */
     public function delete(Child $child)
     {
         $child->delete();
