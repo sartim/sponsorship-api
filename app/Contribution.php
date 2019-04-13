261f8f7cddd37e48c1f0d03874f8257370562418
Tim <sarrtim@gmail.com>
2019-04-13 18:06:40 +0300

    Add model methods

--- app/Contribution.php
+++ app/Contribution.php
@@ -3,16 +3,68 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Contribution extends Model
 {
+
     protected $fillable = ['child_id', 'sponsor_id', 'currency_id', 'contribution'];
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function sponsor(){
         return $this->belongsTo('App\Sponsor');
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function child(){
         return $this->belongsTo('App\Child');
+    }
+
+    /**
+     * Retrieve monthly contributions
+     * @return array
+     */
+    public function getMonthly()
+    {
+        $data = DB::table('contributions')
+            ->select(DB::raw("SUM(contribution) AS contribution, extract(year from created_at) AS year"))
+            ->groupBy(DB::raw("year"))
+            ->get();
+        $output = array();
+        foreach($data as $obj){
+            array_push($output, array(
+                    'contribution' =>intval($obj->contribution),
+                    'year' => $obj->year
+                )
+            );
+
+        }
+        return $output;
+    }
+
+    /**
+     * Retrieve yearly contributions
+     * @return array
+     */
+    public function getYearly() {
+        $data = DB::table('contributions')
+            ->select(DB::raw("SUM(contribution) AS contribution, extract(year from created_at) AS year"))
+            ->groupBy(DB::raw("year"))
+            ->get();
+
+        $output = array();
+        foreach($data as $obj){
+            array_push($output, array(
+                    'contribution' =>intval($obj->contribution),
+                    'year' => $obj->year
+                )
+            );
+
+        }
+        return $output;
     }
 }
