4b730d23603c622a0cbb96a5822155df7a6587f4
Tim <sarrtim@gmail.com>
2019-04-13 14:32:23 +0300

    Add monthly contribution method

--- app/Http/Controllers/ContributionController.php
+++ app/Http/Controllers/ContributionController.php
@@ -2,19 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Child;
 use App\Contribution;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ContributionController extends Controller
 {
     public function index()
     {
-        return Contribution::all();
+        $contributions = Contribution::with(['sponsor','child'])->get();
+        return $contributions;
     }
 
-    public function show(Contribution $contribution)
+    public function showChildMonthlyContribution(Child $child)
     {
-        return $contribution;
+        $data = DB::table('contributions')
+            ->select(DB::raw("SUM(contribution) as contribution, to_char(to_timestamp (date_part('month', created_at)::text, 'MM'), 'TMmon') AS month"))
+            ->whereRaw('child_id = ?',[$child['id']])
+            ->groupBy(DB::raw("month"))
+            ->get();
+
+        $output = array();
+        foreach($data as $obj){
+            array_push($output, array(
+                'contribution' =>intval($obj->contribution),
+                'month' => $obj->month
+                )
+            );
+
+        }
+        return $output;
     }
 
     public function store(Request $request)
@@ -24,7 +42,7 @@ class ContributionController extends Controller
         return response()->json($contribution, 201);
     }
 
-    public function update(Request $request, Gender $contribution)
+    public function update(Request $request, Contribution $contribution)
     {
         $contribution->update($request->all());
 
